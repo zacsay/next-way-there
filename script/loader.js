9ecee965e5b4812b15e79685b386121cbf0e9037
Zac Sayle <113485555+zacsay@users.noreply.github.com>
2025-02-12 17:06:27 +1100

    Add ability to load region metadata and appropriate notice boxes

--- script/loader.js
+++ script/loader.js
@@ -1,16 +1,26 @@
 const knownRegions = ["victoria"];
 
 function loadRegions() {
+    let regionConfigurations = {};
     knownRegions.forEach(region => {
         const request = new XMLHttpRequest();
         request.open("GET", `https://zacsay.github.io/next-way-there/data/regions/${region}.json`, false);
         request.setRequestHeader("Accept", "application/json");
         request.send();
         if (request.status !== 200) {
-            alert(`Unable to retrieve region metadata for ${region}. The server responded with: ${request.status} ${request.statusText}.`)
-            console.error(request)
+            alert(`Unable to retrieve region metadata for ${region}. The server responded with: ${request.status} ${request.statusText}.`);
+            console.error(request);
+        } else {
+            regionConfigurations[region] = JSON.parse(request.responseText);
         }
     });
+    return regionConfigurations;
 }
 
-loadRegions();
+function saveRegions() {
+    localStorage.setItem("nwtRegionConfigurations", JSON.stringify(loadRegions()));
+}
+
+if (!localStorage.getItem("nwtRegionConfigurations")) {
+    document.getElementById("missing-all-data").removeAttribute("hidden");
+}
