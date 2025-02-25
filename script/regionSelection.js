a7a335aa27ece2c1ea50535f45e699a039b9dfd8
Zac Sayle <113485555+zacsay@users.noreply.github.com>
2025-02-25 16:34:32 +1100

    Add skeleton for possible custom region setup

--- script/regionSelection.js
+++ script/regionSelection.js
@@ -1,6 +1,15 @@
+const customRegionOptions = document.getElementById("custom-region-options");
+
 function updateRegion() {
     const regionDropdown = document.getElementById("region-dropdown");
     const region = regionDropdown.value;
+
     console.log(`Changed to ${region} region.`);
     localStorage.setItem("nwtLastUsedRegion", region);
+
+    if (region === "custom") {
+        customRegionOptions.removeAttribute("hidden")
+    } else {
+        customRegionOptions.setAttribute("hidden", "")
+    }
 }
