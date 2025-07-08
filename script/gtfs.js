5f86c6d6d7f66e2df5fbdffc779f0c8040bab45d
Zac Sayle <113485555+zacsay@users.noreply.github.com>
2025-07-08 15:45:24 +1000

    Store new data in the database

--- script/gtfs.js
+++ script/gtfs.js
@@ -1,6 +1,16 @@
 async function saveToIndexedDB(feedDir, regionName) {
     // Initialise the database connection
     const request = indexedDB.open(`nwtRegionDatabase-${regionName}`, 1);
+    // Map object stores to corresponding GTFS files
+    const OSToGTFS = {
+        "agencies": "agency.txt",
+        "stops": "stops.txt",
+        "routes": "routes.txt",
+        "trips": "trips.txt",
+        "stop_times": "stop_times.txt",
+        "calendar": "calendar.txt",
+        "calendar_dates": "calendar_dates.txt"
+    };
     let db;
 
     request.onerror = () => {
@@ -47,78 +57,104 @@ async function saveToIndexedDB(feedDir, regionName) {
 
         calendarDatesOS.createIndex("date", "date");
     };
-    request.onsuccess = (event) => {
+
+    request.onsuccess = async (event) => {
         db = event.target.result;
-        db.onerror = (event) => {
-            console.error(`Database error: ${event.target.error?.message}`);
+
+        db.onerror = (event) => { // Catch-all error handler
+            throw new Error(`Database error: ${event.target.error?.message}`);
         };
+
+        const fileHandles = await getGTFSFileHandles();
+
+        for (const [store, fileName] of Object.entries(OSToGTFS)) {
+            const file = await fileHandles[fileName].getFile().then();
+
+            Papa.parse(file, {
+                header: true, skipEmptyLines: true, complete: (parsed) => {
+                    console.log(store)
+                    const transaction = db.transaction([store], "readwrite");
+                    const objectStore = transaction.objectStore(store);
+
+                    for (const obj of parsed.data) {
+                        objectStore.add(obj);
+                    }
+                }
+            });
+        }
     };
 
-    // Discover the required files used
-    const possible = [
-        "agency.txt",
-        "stops.txt",
-        "routes.txt",
-        "trips.txt",
-        "stop_times.txt",
-        "calendar.txt",
-        "calendar_dates.txt",
-        "fare_attributes.txt",
-        "fare_rules.txt",
-        "timeframes.txt",
-        "fare_media.txt",
-        "fare_products.txt",
-        "fare_leg_rules.txt",
-        "fare_leg_join_rules.txt",
-        "fare_transfer_rules.txt",
-        "areas.txt",
-        "stop_areas.txt",
-        "networks.txt",
-        "route_networks.txt",
-        "shapes.txt",
-        "frequencies.txt",
-        "transfers.txt",
-        "pathways.txt",
-        "levels.txt",
-        "location_groups.txt",
-        "location_groups_stops.txt",
-        "locations.geojson",
-        "booking_rules.txt",
-        "translations.txt",
-        "feed_info.txt",
-        "attributions.txt"
-    ];
-    let fileHandles = {};
+    request.onerror = (event) => {
+        throw new Error(`Database connection error: ${request.error}`);
+    };
 
-    for (const file of possible) {
-        let fileHandle;
+    async function getGTFSFileHandles() {
+        // Discover the required files used
+        const possible = [
+            "agency.txt",
+            "stops.txt",
+            "routes.txt",
+            "trips.txt",
+            "stop_times.txt",
+            "calendar.txt",
+            "calendar_dates.txt",
+            "fare_attributes.txt",
+            "fare_rules.txt",
+            "timeframes.txt",
+            "fare_media.txt",
+            "fare_products.txt",
+            "fare_leg_rules.txt",
+            "fare_leg_join_rules.txt",
+            "fare_transfer_rules.txt",
+            "areas.txt",
+            "stop_areas.txt",
+            "networks.txt",
+            "route_networks.txt",
+            "shapes.txt",
+            "frequencies.txt",
+            "transfers.txt",
+            "pathways.txt",
+            "levels.txt",
+            "location_groups.txt",
+            "location_groups_stops.txt",
+            "locations.geojson",
+            "booking_rules.txt",
+            "translations.txt",
+            "feed_info.txt",
+            "attributions.txt"
+        ];
+        let fileHandles = {};
 
-        try {
-            fileHandle = await feedDir.getFileHandle(file);
-        } catch (NotFoundError) {
-            continue;
+        for (const file of possible) {
+            let fileHandle;
+
+            try {
+                fileHandle = await feedDir.getFileHandle(file);
+            } catch (NotFoundError) {
+                continue;
+            }
+
+            fileHandles[file] = fileHandle;
         }
+        console.log(fileHandles);
 
-        fileHandles[file] = fileHandle;
-    }
-    console.log(fileHandles);
-
-    // Verify that existence requirements are met
-    if (fileHandles["agency.txt"] === undefined || fileHandles["routes.txt"] === undefined || fileHandles["trips.txt"] === undefined || fileHandles["stop_times.txt"] === undefined) {
-        throw new Error("GTFS feed is completely invalid: missing agency.txt, routes.txt, trips.txt, and/or stop_times.txt");
-    }
-    if (fileHandles["calendar.txt"] === undefined && fileHandles["calendar_dates.txt"] === undefined) {
-        throw new Error("GTFS feed is completely invalid: neither calendar.txt nor calendar_dates.txt exist");
-    }
-    if (fileHandles["translations.txt"] !== undefined && fileHandles["feed_info.txt"] === undefined) {
-        throw new Error("GTFS feed is completely invalid: translations.txt exists, but feed_info.txt does not");
-    }
-    if (fileHandles["stops.txt"] === undefined && fileHandles["locations.geojson"] === undefined) {
-        throw new Error("GTFS feed is completely invalid: neither stops.txt nor locations.geojson exist");
-    } else
-        if (fileHandles["stops.txt"] === undefined) {
+        // Verify that existence requirements are met
+        if (fileHandles["agency.txt"] === undefined || fileHandles["routes.txt"] === undefined || fileHandles["trips.txt"] === undefined || fileHandles["stop_times.txt"] === undefined) {
+            throw new Error("GTFS feed is completely invalid: missing agency.txt, routes.txt, trips.txt, and/or stop_times.txt");
+        }
+        if (fileHandles["calendar.txt"] === undefined && fileHandles["calendar_dates.txt"] === undefined) {
+            throw new Error("GTFS feed is completely invalid: neither calendar.txt nor calendar_dates.txt exist");
+        }
+        if (fileHandles["translations.txt"] !== undefined && fileHandles["feed_info.txt"] === undefined) {
+            throw new Error("GTFS feed is completely invalid: translations.txt exists, but feed_info.txt does not");
+        }
+        if (fileHandles["stops.txt"] === undefined && fileHandles["locations.geojson"] === undefined) {
+            throw new Error("GTFS feed is completely invalid: neither stops.txt nor locations.geojson exist");
+        } else if (fileHandles["stops.txt"] === undefined) {
             throw new Error("GTFS feed is valid but not supported: stops.txt does not exist");
         }
 
-    console.log("Feed passed all validation checks");
+        console.log("Feed passed all validation checks");
+        return fileHandles;
+    }
 }
