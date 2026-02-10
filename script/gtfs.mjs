b59155a4a85ed958a8bb33caef18363b0cbb690f
Zac Sayle <113485555+zacsay@users.noreply.github.com>
2026-02-10 16:31:58 +1100

    Add classes for GTFS feed elements

--- script/gtfs.mjs
+++ script/gtfs.mjs
@@ -1,3 +1,271 @@
+/** Class for whole GTFS feeds */
+class Feed {
+    /** @type {Record<string, Agency>} */
+    agencies;
+    /** @type {Record<string, Stop>} */
+    stops;
+    /** @type {Record<string, Route>} */
+    routes;
+    /** @type {Record<string, Trip>} */
+    trips;
+    /** @type {StopTime[]} */
+    stopTimes;
+    /** @type {Record<string, Calendar>} */
+    calendars;
+    /** @type {CalendarDate[]} */
+    calendarDates;
+}
+
+/** Class for GTFS agencies */
+class Agency {
+    /** @type {string} */
+    name;
+    /** @type {URL} */
+    url;
+    /** @type {string} */
+    timezone;
+}
+
+/** Class for GTFS stops */
+class Stop {
+    /** @type {string | null} */
+    code;
+    /** @type {string | null} */
+    name;
+    /** @type {string | null} */
+    ttsName;
+    /** @type {number} */
+    latitude;
+    /** @type {number} */
+    longitude;
+    /** @type {string | null} */
+    zone;
+    /** @type {URL | null} */
+    url;
+    /** @type {LocationType} */
+    locationType = LocationType.PLATFORM;
+    /** @type {Stop | null} */
+    parentStation;
+    /** @type {WheelchairBoarding} */
+    wheelchairBoarding = WheelchairBoarding.NO_INFORMATION;
+    /** @type {string | null} */
+    platformCode;
+}
+
+/**
+ * Location types for stops
+ * @enum {number}
+ * @readonly
+ */
+const LocationType = {
+    PLATFORM: 0,
+    STATION: 1,
+    ENTRANCE_EXIT: 2,
+    GENERIC_NODE: 3,
+    BOARDING_AREA: 4
+};
+
+/**
+ * Wheelchair accessibility information for stops
+ * @enum {number}
+ * @readonly
+ */
+const WheelchairBoarding = {
+    NO_INFORMATION: 0,
+    WHEELCHAIR_ACCESSIBLE: 1,
+    NOT_WHEELCHAIR_ACCESSIBLE: 2
+};
+
+/** Class for GTFS routes */
+class Route {
+    /** @type {Agency} */
+    agency;
+    /** @type {string | null} */
+    shortName;
+    /** @type {string | null} */
+    longName;
+    /** @type {RouteType} */
+    type;
+    /** @type {URL | null} */
+    url;
+    /** @type {string} */
+    color = "#ffffff";
+    /** @type {string} */
+    textColor = "#000000";
+    /** @type {number | null} */
+    sortOrder;
+}
+
+/**
+ * Route type information
+ * @enum {number}
+ * @readonly
+ */
+const RouteType = {
+    TRAM: 0,
+    SUBWAY: 1,
+    RAIL: 2,
+    BUS: 3,
+    FERRY: 4,
+    CABLE_TRAM: 5,
+    AERIAL: 6,
+    FUNICULAR: 7,
+    TROLLEYBUS: 11,
+    MONORAIL: 12
+};
+
+/** Class for GTFS trips */
+class Trip {
+    /** @type {Route} */
+    route;
+    /** @type {Calendar | CalendarDate} */
+    service;
+    /** @type {string | null} */
+    headsign;
+    /** @type {string | null} */
+    shortName;
+    /** @type {Direction | null} */
+    direction;
+    /** @type {string | null} */
+    block;
+    /** @type {WheelchairAccessible} */
+    wheelchairAccessible = WheelchairAccessible.NO_INFORMATION;
+    /** @type {BikesAllowed} */
+    bikesAllowed = BikesAllowed.NO_INFORMATION;
+}
+
+/** 
+ * Trip direction information.
+ * 
+ * Note: outbound and inbound chosen based on the examples in the GTFS documentation.
+ * There is no obligation on feed producers to honour these directions, 
+ * and thus they cannot be relied on as being correct in this sense.
+ * @enum {number}
+ * @readonly
+ */
+const Direction = {
+    OUTBOUND: 0,
+    INBOUND: 1
+};
+
+/**
+ * Wheelchair accessibility information for trips
+ * @enum {number}
+ * @readonly
+ */
+const WheelchairAccessible = {
+    NO_INFORMATION: 0,
+    WHEELCHAIR_ACCESSIBLE: 1,
+    NOT_WHEELCHAIR_ACCESSIBLE: 2
+};
+
+/**
+ * Bike permissibility information for trips
+ * @enum {number}
+ * @readonly
+ */
+const BikesAllowed = {
+    NO_INFORMATION: 0,
+    BIKES_ALLOWED: 1,
+    NO_BIKES_ALLOWED: 2
+};
+
+/** Class for GTFS stop times */
+class StopTime {
+    /** @type {Trip} */
+    trip;
+    /** @type {Time | null} */
+    arrivalTime;
+    /** @type {Time | null} */
+    departureTime;
+    /** @type {Stop} */
+    stop;
+    /** @type {number} */
+    sequence;
+    /** @type {string | null} */
+    headsign;
+    /** @type {PickUpDropOffType} */
+    pickUpType = PickUpDropOffType.REGULAR;
+    /** @type {PickUpDropOffType} */
+    dropOffType = PickUpDropOffType.REGULAR;
+    /** @type {boolean} */
+    timepoint = true;
+}
+
+/** Class for times which do not depend on dates */
+class Time {
+    /**
+     * Takes a time encompassing a relative date, as used in GTFS, 
+     * and creates a more conventional time.
+     * The relative date is stored in the `dateAdjustment` property.
+     * All numbers will be truncated if they are non-integral.
+     * @param {number} hour Must be greater than or equal to 0
+     * @param {number} minute Must be in range [0, 60)
+     * @param {number} second Must be in range [0, 60)
+     */
+    constructor(hour, minute, second) {
+        if (hour < 0 || minute < 0 || second < 0 || hour === Infinity || minute >= 60 || second >= 60) {
+            throw new Error(`Can't convert *${hour}:${minute}:${second} to a Time object`);
+        }
+        this.hour = Math.trunc(hour) % 24;
+        this.minute = Math.trunc(minute);
+        this.second = Math.trunc(second);
+        this.dateAdjustment = Math.trunc(hour / 24);
+    }
+}
+
+/** 
+ * Pick up or drop off type information for stop times
+ * @enum {number}
+ * @readonly
+ */
+const PickUpDropOffType = {
+    REGULAR: 0,
+    NONE: 1,
+    PHONE_AGENCY: 2,
+    COORDINATE_WITH_DRIVER: 3
+};
+
+class Calendar {
+    /** @type {boolean} */
+    monday;
+    /** @type {boolean} */
+    tuesday;
+    /** @type {boolean} */
+    wednesday;
+    /** @type {boolean} */
+    thursday;
+    /** @type {boolean} */
+    friday;
+    /** @type {boolean} */
+    saturday;
+    /** @type {boolean} */
+    sunday;
+    /** @type {Date} */
+    startDate;
+    /** @type {Date} */
+    endDate;
+}
+
+class CalendarDate {
+    /** @type {Calendar | string} */
+    service;
+    /** @type {Date} */
+    date;
+    /** @type {ExceptionType} */
+    exceptionType;
+}
+
+/**
+ * Exception types for calendar dates
+ * @enum {number}
+ * @readonly
+ */
+const ExceptionType = {
+    SERVICE_ADDED: 1,
+    SERVICE_REMOVED: 2
+};
+
 /**
  * Saves the data in the directory to the IndexedDB
  * @param {FileSystemDirectoryHandle} feedDir The directory containing the data to save
